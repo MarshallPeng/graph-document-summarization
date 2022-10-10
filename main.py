14981cb7f9f4785996ce2d3e430765babfdaee3c
MarshallPeng <marshall.s.peng.20@dartmouth.edu>
2022-10-09 23:07:17 -0700

    Gamma, K, and zero out negative values change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from embeddings.sbert import SBert
 from loader.load import SQuALITYLoader
-from sampler.cohen_lewis import find_similar_pairs
+from sampler.cohen_lewis import find_similar_pairs, CohenLewis
 import numpy as np
 from random import randint
 
@@ -26,8 +26,20 @@ for doc_id in id_to_embeddings:
     d = A.shape[0]
     n = A.shape[1]
 
-    K = d ** 0.25
+    # zero out negative values
+    A[A < 0] = 0
 
+    K = 0.5
     R = find_similar_pairs(A, K)
 
-    print(R)
+    greater_than = set()
+    less_than = set()
+
+    for pair in R:
+        if R[pair]['dot_product'] >= K:
+            greater_than.add(pair)
+        else:
+            less_than.add(pair)
+
+    print(f'num greater than: {len(greater_than)}')
+    print(f'num less than: {len(less_than)}')
--- sampler/cohen_lewis.py
+++ sampler/cohen_lewis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+from tqdm import tqdm
 
 
 class CohenLewis:
@@ -9,9 +10,10 @@ class CohenLewis:
         self.A = A
         self.d = A.shape[0]
         self.n = A.shape[1]
-        self.gram_matrix = None
+        self.H = 100
+        self.gram_matrix = self.A.T @ self.A
         self.delta = 0.01
-        self.gamma = np.sum(self.A)
+        self.gamma = np.sum(self.gram_matrix)
         self.scores = self._preprocess()  # dim 1 x d
 
     def _preprocess(self):
@@ -29,11 +31,6 @@ class CohenLewis:
         """
         return self.scores[r]
 
-    def gram_matrix(self):
-        if self.gram_matrix is None:
-            self.gram_matrix = self.A.T @ self.A
-        return self.gram_matrix
-
     def cohen_lewis(self):
         """
         Return (i,j) with probability proportional to (A^T A)_{ij}
@@ -45,13 +42,17 @@ class CohenLewis:
         # Sample i with probability \frac{A_{ri}}{score(r)}
         i = random.choices(np.arange(self.n), weights=np.squeeze(self.A[r]))[0]
 
+        # eliminate probability of j == i
+        j_prob = np.squeeze(self.A[r])
+        j_prob[i] = 0
+
         # Sample j with probability \frac{A_{rj}}{score(r)}
-        j = random.choices(np.arange(self.n), weights=np.squeeze(self.A[r]))[0]
+        j = random.choices(np.arange(self.n), weights=j_prob)[0]
 
         return (i, j) if j >= i else (j, i)
 
 
-def find_similar_pairs(A, K):
+def find_similar_pairs(A, K=None):
     """
     Return pairs of columns of A with dot product >= K
     :param A:
@@ -59,18 +60,13 @@ def find_similar_pairs(A, K):
     :return:
     """
     sampler = CohenLewis(A)
+    K = sampler.gamma / sampler.H if K is None else K
     N = math.ceil((sampler.gamma / K) * math.log(sampler.gamma / (K * sampler.delta)))
     R = {}
 
     print(f'Sampling {N} pairs')
-    for i in range(N):
-        if i % 1000 == 0:
-            print(f'{i} / {N}')
-
+    for i in tqdm(range(N), position=0, leave=True):
         sample = sampler.cohen_lewis()
-
-        if sample[0] == sample[1]:
-            continue
 
         if sample not in R:
             R[sample] = {}
